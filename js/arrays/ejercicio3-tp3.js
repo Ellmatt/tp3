6f5ecfaf9a9e86227ec775d8846da47e0ad87463
matias <pagosptcmatias@gmail.com>
2022-07-19 18:37:44 -0300

    agg ej 3

--- js/arrays/ejercicio3-tp3.js
+++ js/arrays/ejercicio3-tp3.js
@@ -1,19 +1,43 @@
-/* Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”.
- Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
+// 3- Escribir un script que simule el lanzamiento de dos dados math.ramdom.
+//  Hacer uso de la función Math.random para obtener números aleatorios entre 1 y 6 para cada uno de los lanzamientos de los dados.
+//  Sumar el resultado de lanzar dos dados y anotar en un array el número de apariciones de dicha suma, repitiendo 50 veces esta operación.
 
-Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp */
+let sumas = [];
+let pruebaDado1 = [];
+let pruebaDado2 = [];
+for (let i = 0; i < 50; i++) {
+  let dado1 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  let dado2 = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+  let suma = dado1 + dado2;
+  sumas.push(suma);
+  pruebaDado1.push(dado1);
+  pruebaDado2.push(dado2);
+}
 
+document.write("<br> resultado de dado 1 " + pruebaDado1);
+document.write("<br> resultado de dado 2 " + pruebaDado2);
+document.write("<br>resultado de la suma " + sumas);
 
-  
-  
-  
+document.write(`<table> 
+<tbody>
+<tr> 
+<td>Resultado</td>
+<td>Frecuencia</td>
+</tr>`);
+for (let resultado = 2; resultado <= 12; resultado++) {
+  for (let posicion = 0; posicion < sumas.length; posicion++) {
+    if (resultado === sumas[posicion]) {
+      //sumar las apariciones
+    }
+  }
+  document.write(`
+<tr>
+<td>${resultado}</td>
+<td>x</td>
+</tr>`);
+}
 
-    do{
-        let texto = prompt("Ingrese un texto de hasta 10 caracteres, tener en cuenta que un espacio cuenta como caracter");
-  console.log(texto);
-  if (texto = true) {
-    document.write('-'+ texto)
-      } 
-      
-    } while (confirm("desea ingresar un texto?")); 
-    
+document.write(`
+</tbody>
+</table>`);
+
