d2a4c16ec3919897236325011812fa4571acc6fe
matias <pagosptcmatias@gmail.com>
2022-07-21 20:37:55 -0300

    agg ej 1 funciones

--- js/funciones/ejercicio1-tp3.js
+++ js/funciones/ejercicio1-tp3.js
@@ -1,3 +1,15 @@
 // 1- Escribir el código de una función a la que se pasa como parámetro un número entero y 
 // devuelve como resultado una cadena de texto que indica si el número es par o impar. 
 // Mostrar por pantalla el resultado devuelto por la función.
+
+function numero(y) {
+    if (y % 2 === 0) {
+       document.write('es par');
+    }else{
+        document.write('es impar')
+    }
+    return numero;
+  }
+  
+  numero(parseInt(prompt('ingrese un numero')));
+  
