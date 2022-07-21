493f89a3f465ef8e8ce6d093499441e1ee07fb84
matias <pagosptcmatias@gmail.com>
2022-07-21 18:17:14 -0300

    agg ej 2

--- js/arrays/ejercicio2-tp3.js
+++ js/arrays/ejercicio2-tp3.js
@@ -1,5 +1,5 @@
 // 2-  Crear un script que solicite al usuario mediante un prompt el nombre de ciudades y
-// almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado, 
+// almacenarlas en un arreglo, cuando el usuario selecciona cancelar se debe mostrar el arreglo generado,
 // luego realizar las siguientes acciones:
 
 // Mostrar la longitud del arreglo.
@@ -8,20 +8,16 @@
 // Escribe por pantalla el elemento que ocupa la segunda posición.
 // Sustituye el elemento que ocupa la segunda posición por la ciudad de 'Barcelona'.
 
-let  ciudades = [];
+let ciudades = [];
 
+do {
+  let prueba = prompt("ingrese una ciudad");
+  if (prueba !== null) {
+    ciudades.push(prueba);
+  }
+  for (let i = 0; i < ciudades.length; i++) {
+    document.write("<br>" + ciudades);
+  }
+} while (confirm("queres agregar mas ciudades?"));
 
-
-do{
-  ciudades.push(prompt('ingrese una ciudad'))
- for( let i = 0; i < ciudades.length; i++){
-  document.write('<br>'+ ciudades)
- }
- 
-}while(confirm('Ciudades agregadas'))
-
-document.write('<br>'+ ciudades.length)
-
-
-
-
+document.write("<br>" + ciudades.length);
