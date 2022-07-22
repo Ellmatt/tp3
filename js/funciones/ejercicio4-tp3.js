9221f449e048f981b6a39df8bb805b003096ae97
matias <pagosptcmatias@gmail.com>
2022-07-22 20:32:17 -0300

    agregue ejercicio 4 de funciones

--- js/funciones/ejercicio4-tp3.js
+++ js/funciones/ejercicio4-tp3.js
@@ -1,3 +1,12 @@
-// 4- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, 
-// la creación de la tabla debe ser realizada con una función y 
+// 4- Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla,
+// la creación de la tabla debe ser realizada con una función y
 // mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
+
+function tablaDeMultiplicar(numero) {
+  for (let i = 1; i < 11; i++) {
+    document.write(numero + " x " + i + " = " + numero * i + "<br>");
+  }
+
+  return tablaDeMultiplicar;
+}
+tablaDeMultiplicar(parseInt(prompt("ingrese un numero")));
