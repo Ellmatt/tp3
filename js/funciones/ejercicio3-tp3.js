6dc0b0ad048b506144fed2af13a5d4ef0d29a17e
matias <pagosptcmatias@gmail.com>
2022-07-22 19:44:05 -0300

    aggregue ejercicio 4 de funciones

--- js/funciones/ejercicio3-tp3.js
+++ js/funciones/ejercicio3-tp3.js
@@ -1,4 +1,22 @@
-// 3- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo, 
+// 3- Solicitar por pantalla al usuario ingresar el valor de los lados de un rectángulo,
 // luego crear una función para calcular su perímetro y mostrarlo por pantalla.
 
 // La fórmula del perímetro  es p = 2*(a +b)
+
+function rectangulo(a, b) {
+  console.log(a);
+  console.log(b);
+  let resultado = 2 * (a + b);
+  console.log(resultado);
+  if(resultado){ 
+     document.write(`El perimetro del rectangulo es ${resultado}`);
+}else{
+    alert('ingrese un valor numerico')
+}
+
+  return rectangulo;
+}
+rectangulo(
+  (a = parseInt(prompt(`Ingrese lado A para calcular su perimetro`))),
+  (b = parseInt(prompt("Ingrese lado B paracalcular su perimetro")))
+);
